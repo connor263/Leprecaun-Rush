3e0e8577da09cca1ad6a0f648d2aba106b5a1ab0
c0nnor263 <bojchu7@gmail.com>
2022-06-01 15:22:01 +0300

    Update

--- app/src/main/java/com/starmakerinteractive/starmak/utils/web/CacliAppetaoderacacerry.kt
+++ app/src/main/java/com/starmakerinteractive/starmak/utils/web/CacliAppetaoderacacerry.kt
@@ -2,9 +2,9 @@ package com.starmakerinteractive.starmak.utils.web
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.provider.Settings
 import androidx.core.content.ContextCompat
+
 
 class CacliAppetaoderacacerry(private val context: Context) {
     val isIntCacliinracacerryonnected: Boolean
@@ -12,15 +12,8 @@ class CacliAppetaoderacacerry(private val context: Context) {
             val conneCacliinatAdasdfasStyletaoderacacerryanager =
                 ContextCompat.getSystemService(context, ConnectivityManager::class.java)
                     ?: return false
-            val acCacliinatAdasdfasStyletaoderacacerrywork = conneCacliinatAdasdfasStyletaoderacacerryanager.activeNetwork ?: return false
-            val neCacliinatAdasdfasStyletaoderacacerrybilities =
-                conneCacliinatAdasdfasStyletaoderacacerryanager.getNetworkCapabilities(acCacliinatAdasdfasStyletaoderacacerrywork) ?: return false
-            return when {
-                neCacliinatAdasdfasStyletaoderacacerrybilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                neCacliinatAdasdfasStyletaoderacacerrybilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                neCacliinatAdasdfasStyletaoderacacerrybilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
+            val netiinatAdasdfasStyletaoderanfo = conneCacliinatAdasdfasStyletaoderacacerryanager.activeNetworkInfo
+            return netiinatAdasdfasStyletaoderanfo != null && netiinatAdasdfasStyletaoderanfo.isConnected
         }
 
     val isCacliinatAdasdfasStyletaoderacacerryoper: Boolean
